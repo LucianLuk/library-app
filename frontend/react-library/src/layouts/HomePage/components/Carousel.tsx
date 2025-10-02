e4dcdc22b2a928a5ac6b37220c81041d24c95011
ElvisLuk <lucianluk@outlook.com>
2025-10-02 23:26:48 +0800

    feat(frontend): Loading Spinner Component

--- frontend/react-library/src/layouts/HomePage/components/Carousel.tsx
+++ frontend/react-library/src/layouts/HomePage/components/Carousel.tsx
@@ -1,6 +1,7 @@
 import {ReturnBook} from "./ReturnBook";
 import {useEffect, useState} from "react";
 import BookModel from "../../../models/BookModel";
+import {SpinnerLoading} from "../../Util/SpinnerLoading";
 
 export const Carousel = () => {
 
@@ -42,9 +43,7 @@ export const Carousel = () => {
 
     if (isLoading) {
         return (
-            <div className='container m-5'>
-                <p>Loading...</p>
-            </div>
+            <SpinnerLoading/>
         );
     }
 
