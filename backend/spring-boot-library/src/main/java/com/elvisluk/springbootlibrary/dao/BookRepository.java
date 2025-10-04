2126fcf17e57fc4d661ebf347eacd79b320f4fb5
ElvisLuk <lucianluk@outlook.com>
2025-10-04 20:05:48 +0800

    feat(backend): search by category

--- backend/spring-boot-library/src/main/java/com/elvisluk/springbootlibrary/dao/BookRepository.java
+++ backend/spring-boot-library/src/main/java/com/elvisluk/springbootlibrary/dao/BookRepository.java
@@ -9,4 +9,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     Page<Book> findAllByTitleContaining(@RequestParam("title") String title, Pageable pageable);
+
+    Page<Book> findByCategory(@RequestParam("category") String category, Pageable pageable);
 }
