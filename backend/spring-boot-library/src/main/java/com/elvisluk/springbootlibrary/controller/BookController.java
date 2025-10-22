c32d9ec86cdd8b458fc9aceb597fbf6fb3e4466f
ElvisLuk <lucianluk@outlook.com>
2025-10-22 14:47:18 +0800

    feat(backend): add Checkout controller and service and add checkoutBook functionality

---
+++ backend/spring-boot-library/src/main/java/com/elvisluk/springbootlibrary/controller/BookController.java
@@ -0,0 +1,26 @@
+package com.elvisluk.springbootlibrary.controller;
+
+import com.elvisluk.springbootlibrary.entity.Book;
+import com.elvisluk.springbootlibrary.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+@RequestMapping("/api/books")
+public class BookController {
+
+    private BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PutMapping("/secure/checkout")
+    public Book checkoutBook(@RequestParam Long bookId) throws Exception {
+        String userEmail = "testuser@email.com";
+        return bookService.checkoutBook(userEmail, bookId);
+    }
+}
---
+++ backend/spring-boot-library/src/main/java/com/elvisluk/springbootlibrary/service/BookService.java
@@ -0,0 +1,48 @@
+package com.elvisluk.springbootlibrary.service;
+
+import com.elvisluk.springbootlibrary.dao.BookRepository;
+import com.elvisluk.springbootlibrary.dao.CheckoutRepository;
+import com.elvisluk.springbootlibrary.entity.Book;
+import com.elvisluk.springbootlibrary.entity.Checkout;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    private CheckoutRepository checkoutRepository;
+
+    public BookService(BookRepository bookRepository, CheckoutRepository checkoutRepository) {
+        this.bookRepository = bookRepository;
+        this.checkoutRepository = checkoutRepository;
+    }
+
+    public Book checkoutBook(String userEmail, Long bookId) throws Exception {
+        Optional<Book> bookOptional = bookRepository.findById(bookId);
+        Checkout validateCheckout = checkoutRepository.findByUserEmailAndBookId(userEmail, bookId);
+
+        if (!bookOptional.isPresent() || validateCheckout != null || bookOptional.get().getCopiesAvailable() <= 0) {
+            throw new Exception("Book cannot be checked out because it is either not available or already checked out by the user.");
+        }
+
+        // TODO - solve concurrency issue when two users try to check out the last available copy of a book
+        // TODO - solve concurrency issue when one user tries to check out the same book multiple times simultaneously
+        Book book = bookOptional.get();
+        book.setCopiesAvailable(book.getCopiesAvailable() - 1);
+        bookRepository.save(book);
+
+        Checkout checkout = new Checkout(userEmail,
+                LocalDate.now().toString(),
+                LocalDate.now().plusDays(7).toString(),
+                book.getId());
+        checkoutRepository.save(checkout);
+
+        return book;
+    }
+}
