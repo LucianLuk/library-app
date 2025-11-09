9ab676d6d72972cb131e01590477a00545b7ca29
ElvisLuk <lucianluk@outlook.com>
2025-11-09 15:13:21 +0800

    feat(frontend): Stripe dependencies and preparations for use of Stripe

--- frontend/react-library/src/index.tsx
+++ frontend/react-library/src/index.tsx
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import {App} from './App';
 import {BrowserRouter} from "react-router-dom";
+import {loadStripe} from "@stripe/stripe-js";
+import {Elements} from "@stripe/react-stripe-js";
+
+const stripePromise = loadStripe('pk_test_51SR7F3Ff3YiKRKhq8DaX7jR12gcWnjeIWBlTfRDKhKzGvwAw76Ovz7v133pBfnQPXjXsfwJzcnI1Ujoiv8BIAF8600qR04i0a0');
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <BrowserRouter>
+        <Elements stripe={stripePromise}>
+            <App/>
+        </Elements>
+    </BrowserRouter>
 );
 
